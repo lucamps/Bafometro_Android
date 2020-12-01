bfca027a40664d4dccc74c82c847db4eef861aad
lucamps <lucas.campos2@ufv.br>
2020-12-01 16:05:44 -0300

    Abordagem A concluída

--- app/src/main/java/android/lucamps/bafometro_android/MainActivity.java
+++ app/src/main/java/android/lucamps/bafometro_android/MainActivity.java
@@ -1,20 +1,23 @@
 package android.lucamps.bafometro_android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_CODE = 100;
 
-    Double peso;
-    String sexo;
-    int copos;
-    String jejum;
+    private double peso;
+    private int copos;
+    private String sexo;
+    private String jejum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         buttonEnviar.setOnClickListener(view -> {
             try {
                 peso = Double.parseDouble(editTextPeso.getText().toString());
-                copos = Integer.getInteger(editTextCopos.getText().toString(),0);
+                copos = Integer.parseInt(editTextCopos.getText().toString());
                 jejum = editTextJejum.getText().toString();
                 sexo = editTextSexo.getText().toString();
 
@@ -39,12 +42,36 @@ public class MainActivity extends AppCompatActivity {
                 it.putExtra("sexo", sexo);
                 it.putExtra("copos", copos);
                 it.putExtra("jejum", jejum);
-                startActivity(it);
+                startActivityForResult(it, REQUEST_CODE);
             }
             catch (NumberFormatException e){
                 Toast.makeText(this,"Favor preencha os campos numéricos",Toast.LENGTH_LONG).show();
             }
-
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data == null) {
+            Log.i("Activity_Result", "nenhuma Intent recebida.");
+        }
+        else if(requestCode == REQUEST_CODE){
+
+            final double taxaAlc = data.getDoubleExtra("taxaAlc",0.0);
+            final boolean alcoolizada = data.getBooleanExtra("alcoolizada",false);
+
+            if(resultCode == 1){
+                Log.i("Activity_Result", "código para exibit Toast recebido.");
+                if(alcoolizada)
+                    Toast.makeText(this, "Taxa de Alcoolemia: " + taxaAlc + "\nClassificação: Pessoa Alcoolizada", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(this, "Taxa de Alcoolemia: " + taxaAlc + "\nClassificação: Pessoa NÃO Alcoolizada", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Log.i("Activity_Result", "código de resultado inválido.");
+            }
+        }
+
     }
 }
--- app/src/main/java/android/lucamps/bafometro_android/SecondActivity.java
+++ app/src/main/java/android/lucamps/bafometro_android/SecondActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    double getCoeficiente(String jejum, String sexo){
+        if(jejum.equals("s") || jejum.equals("S")){
+            if(sexo.equals("m") || sexo.equals("M"))
+                return 0.7;
+            else
+                return 0.6;
+        }
+        return 1.1;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +25,28 @@ public class SecondActivity extends AppCompatActivity {
 
         Intent it = getIntent();
 
-        final Double peso = it.getDoubleExtra("peso",1.0);
+        final double peso = it.getDoubleExtra("peso",1.0);
         final String sexo = it.getStringExtra("sexo");
         final int copos = it.getIntExtra("copos",0);
         final String jejum = it.getStringExtra("jejum");
 
-        Toast.makeText(this, "Peso: " + peso + ", sexo: "
-                + sexo + ", copos: " + copos +", jejum: " + jejum , Toast.LENGTH_LONG).show();
+        Button buttonCalcular = findViewById(R.id.buttonCalcular);
 
+        buttonCalcular.setOnClickListener(view -> {
+            double taxaAlc = (copos * 4.8) / (peso * getCoeficiente(jejum,sexo) );
+            boolean alcoolizada = false;
+
+            if(taxaAlc > 0.2)
+                alcoolizada = true;
+
+            Intent itResult = new Intent();
+
+            itResult.putExtra("taxaAlc",taxaAlc);
+            itResult.putExtra("alcoolizada",alcoolizada);
+
+            setResult(1, itResult);
+
+            finish();
+        });
     }
 }
