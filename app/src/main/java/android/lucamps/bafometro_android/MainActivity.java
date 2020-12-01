94664c390fcf1b7f6e0e44ebf65ecb58108358cc
lucamps <lucas.campos2@ufv.br>
2020-12-01 12:48:42 -0300

    Mandando as informações da activity principal para a secundária + tratamento de exceção para valores numéricos

--- app/src/main/java/android/lucamps/bafometro_android/MainActivity.java
+++ app/src/main/java/android/lucamps/bafometro_android/MainActivity.java
@@ -2,13 +2,49 @@ package android.lucamps.bafometro_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Double peso;
+    String sexo;
+    int copos;
+    String jejum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextPeso = findViewById(R.id.editTextPeso);
+        EditText editTextSexo = findViewById(R.id.editTextSexo);
+        EditText editTextCopos = findViewById(R.id.editTextCopos);
+        EditText editTextJejum = findViewById(R.id.editTextJejum);
+        Button buttonEnviar = findViewById(R.id.buttonEnviarDados);
+
+        buttonEnviar.setOnClickListener(view -> {
+            try {
+                peso = Double.parseDouble(editTextPeso.getText().toString());
+                copos = Integer.getInteger(editTextCopos.getText().toString(),0);
+                jejum = editTextJejum.getText().toString();
+                sexo = editTextSexo.getText().toString();
+
+                Intent it = new Intent(getBaseContext(), SecondActivity.class);
+                it.putExtra("peso", peso);
+                it.putExtra("sexo", sexo);
+                it.putExtra("copos", copos);
+                it.putExtra("jejum", jejum);
+                startActivity(it);
+            }
+            catch (NumberFormatException e){
+                Toast.makeText(this,"Favor preencha os campos numéricos",Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
--- app/src/main/java/android/lucamps/bafometro_android/SecondActivity.java
+++ app/src/main/java/android/lucamps/bafometro_android/SecondActivity.java
@@ -2,7 +2,9 @@ package android.lucamps.bafometro_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second2);
+
+        Intent it = getIntent();
+
+        final Double peso = it.getDoubleExtra("peso",1.0);
+        final String sexo = it.getStringExtra("sexo");
+        final int copos = it.getIntExtra("copos",0);
+        final String jejum = it.getStringExtra("jejum");
+
+        Toast.makeText(this, "Peso: " + peso + ", sexo: "
+                + sexo + ", copos: " + copos +", jejum: " + jejum , Toast.LENGTH_LONG).show();
+
     }
 }
