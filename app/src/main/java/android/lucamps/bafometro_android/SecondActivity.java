a741c1d9dcafa30f23e7dd0c8d206b29f744f150
lucamps <lucas.campos2@ufv.br>
2020-12-01 10:25:52 -0300

    Desenhando segunda tela

---
+++ app/src/main/java/android/lucamps/bafometro_android/SecondActivity.java
@@ -0,0 +1,14 @@
+package android.lucamps.bafometro_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+    }
+}
